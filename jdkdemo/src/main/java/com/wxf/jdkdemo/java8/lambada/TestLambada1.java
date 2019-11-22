208e6ab87f09f3572e5de98aa67e7baa00bc2d56
wangxuefei <wangxuefei@staff.hexun.com>
2019-11-22 17:52:58 +0800

    lambda1

---
+++ jdkdemo/src/main/java/com/wxf/jdkdemo/java8/lambada/TestLambada1.java
@@ -0,0 +1,23 @@
+package com.wxf.jdkdemo.java8.lambada;
+
+import com.wxf.jdkdemo.JdkdemoApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @ClassName TestLambada1
+ * @Description
+ * @Author wangxuefei
+ * @Date 2019/11/22 17:35
+ * @Version V1.0
+ **/
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = JdkdemoApplication.class)
+public class TestLambada1 {
+
+    public void  test1(){
+        
+    }
+}
---
+++ jdkdemo/src/test/java/com/wxf/jdkdemo/JdkdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wxf.jdkdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdkdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
